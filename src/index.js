6281fe972a26723cff4960bdb4e0aead8d8c4627
Victoria Ding <victoriading19@gmail.com>
2020-02-01 11:32:58 -0800

    log marker coordinates

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ class Application extends React.Component {
   }
 
   componentDidMount() {
-    console.log(geodata);
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -51,8 +50,11 @@ class Application extends React.Component {
     .addTo(map);
 
     function onDragEnd() {
-      var lngLat = markerA.getLngLat();
-      console.log('Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat);
+      var lngLatA = markerA.getLngLat();
+      console.log('A: Longitude: ' + lngLatA.lng + '<br />Latitude: ' + lngLatA.lat);
+
+      var lngLatB = markerB.getLngLat();
+      console.log('B: Longitude: ' + lngLatB.lng + '<br />Latitude: ' + lngLatB.lat);
     }
 
     markerA.on('dragend', onDragEnd);
