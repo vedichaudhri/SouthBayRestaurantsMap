27f55261ebaecce6141a16e2fd464f2de2dbb2ba
Victoria Ding <victoriading19@gmail.com>
2020-02-05 01:01:38 -0800

    set bounds for map; create an initial circle

--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ import * as serviceWorker from './serviceWorker';
 //import { csv2geojson } from 'csv2geojson';
 import mapboxgl from 'mapbox-gl';
 import geodata from './geodata.js';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
 //import data from 'data.geojson';
+import {
+  CircleMode,
+  DragCircleMode,
+  DirectMode,
+  SimpleSelectMode,
+} from 'mapbox-gl-draw-circle';
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoidmVkaWNoYXVkaHJpIiwiYSI6ImNrNjM2NGpxYjAxaG8zbW1weTNuMmxydDkifQ.2Va6SpC54bm8IkG0SGZ8lw';
+'pk.eyJ1IjoidmVkaWNoYXVkaHJpIiwiYSI6ImNrNjM2NGpxYjAxaG8zbW1weTNuMmxydDkifQ.2Va6SpC54bm8IkG0SGZ8lw';
 
 class Application extends React.Component {
   constructor(props) {
@@ -23,13 +30,19 @@ class Application extends React.Component {
   }
 
   componentDidMount() {
-    console.log(geodata)
+    var bounds = [
+    [-123.0397, 37.024], // Southwest coordinates
+    [-120.881, 37.933033] // Northeast coordinates
+    ];
+
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/light-v10',
       center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
+      zoom: this.state.zoom,
+      maxBounds: bounds
     });
+
     map.on('move', () => {
       this.setState({
         lng: map.getCenter().lng.toFixed(4),
@@ -55,57 +68,88 @@ class Application extends React.Component {
             layout: {
               'icon-image': 'circle',
               'icon-size': 0.007,
-              'text-field': ['get', 'Name'],
-              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-              'text-offset': [0, 0.6]
+              // 'text-field': ['get', 'Name'],
+              // 'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              // 'text-offset': [0, 0.6]
             }
           });
         }
-      );
+        );
     });
 
+    this.handleMarkers(map);
+
+
+    // userProperties has to be enabled
+    const draw = new MapboxDraw({
+      defaultMode: "draw_circle",
+      userProperties: true,
+      modes: {
+        ...MapboxDraw.modes,
+        draw_circle  : CircleMode,
+        drag_circle  : DragCircleMode,
+        direct_select: DirectMode,
+        simple_select: SimpleSelectMode
+      }
+    });
+
+    // Add this draw object to the map when map loads
+    map.addControl(draw);
+    draw.changeMode('draw_circle', { initialRadiusInKm: 10 });
+  }
+
+  handleMarkers(map) {
     var markerA = new mapboxgl.Marker({
       draggable: true
     })
-      .setLngLat([-122.1430195, 37.44])
-      .addTo(map);
+    .setLngLat([-122.1430195, 37.44])
+    .addTo(map);
 
     var markerB = new mapboxgl.Marker({
       draggable: true
     })
-      .setLngLat([-122.2, 37.39])
-      .addTo(map);
+    .setLngLat([-122.2, 37.39])
+    .addTo(map);
 
     function onDragEnd() {
       var lngLatA = markerA.getLngLat();
-      console.log('A: Longitude: ' + lngLatA.lng + '<br />Latitude: ' + lngLatA.lat);
-
       var lngLatB = markerB.getLngLat();
-      console.log('B: Longitude: ' + lngLatB.lng + '<br />Latitude: ' + lngLatB.lat);
     }
 
     markerA.on('dragend', onDragEnd);
     markerB.on('dragend', onDragEnd);
   }
 
+
+  // getIntersect(map, markerA, markerB) {
+  //   let radiusA = 10
+  //   let radiusB = 10
+
+  //   let lngLatA = markerA.getPosition()
+  //   let lngLatB = markerB.getPosition()
+
+
+  // }
+
+  
+
   render() {
     return (
       <div>
-        <div className="sidebarStyle">
-          <div>
-            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
-            {this.state.zoom}
-          </div>
-        </div>
-        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+      <div className="sidebarStyle">
+      <div>
+      Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
+      {this.state.zoom}
       </div>
-    );
+      </div>
+      <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+      </div>);
+    }
   }
-}
 
-ReactDOM.render(<Application />, document.getElementById('app'));
+  ReactDOM.render(<Application />, document.getElementById('app'));
 
-// If you want your app to work offline and load faster, you can change
+  // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
