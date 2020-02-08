7329acc07d086c1e3db8ac2d8fdc98f03e4eb782
Victoria Ding <victoriading19@gmail.com>
2020-02-08 13:47:09 -0800

    push updates

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import mapboxgl from 'mapbox-gl';
 import geodata from './geodata.js';
 import MapboxCircle from 'mapbox-gl-circle';
+import * as turf from '@turf/turf'
 
 mapboxgl.accessToken =
 'pk.eyJ1IjoidmVkaWNoYXVkaHJpIiwiYSI6ImNrNjM2NGpxYjAxaG8zbW1weTNuMmxydDkifQ.2Va6SpC54bm8IkG0SGZ8lw';
@@ -20,17 +21,27 @@ class Application extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleCircles = this.handleCircles.bind(this);
+    this.getIntersectionPoints = this.getIntersectionPoints.bind(this);
+    this.handleMarkers = this.handleMarkers.bind(this);
     this.state = {
       lng: -122.1430195,
       lat: 37.4418834,
-      zoom: 9
+      zoom: 9,
+      circleA_center: [-122.1430195, 37.44],
+      circleB_center: [-122.2, 37.39],
+      circleA_radius: 7000,
+      circleB_radius: 7000,
+      intersectionPoints: this.getIntersectionPoints([-122.1430195, 37.44], 7000, [-122.2, 37.39], 7000),
     };
+    
   }
 
   componentDidMount() {
+    console.log(this.state);
     var bounds = [
-    [-123.0397, 37.024], // Southwest coordinates
-    [-120.881, 37.933033] // Northeast coordinates
+      [-123.0397, 37.024], // Southwest coordinates
+      [-120.881, 37.933033] // Northeast coordinates
     ];
 
     const map = new mapboxgl.Map({
@@ -57,6 +68,7 @@ class Application extends React.Component {
         type: 'geojson',
         data: geodata
       });
+
       map.addLayer({
         'id': 'points',
         'type': 'circle',
@@ -99,33 +111,56 @@ class Application extends React.Component {
       map.getCanvas().style.cursor = '';
     });
 
-    this.handleMarkers(map);
+    // this.handleMarkers(map);
     this.handleCircles(map);
   }
 
   // radius in meters
   handleCircles(map) {
-    var circleA = new MapboxCircle({lat: this.INIT_COORDS_A[1], lng: this.INIT_COORDS_A[0]}, 7000, {
+    var circleA = new MapboxCircle({lat: this.state.circleA_center[1], lng: this.state.circleA_center[0]}, this.state.circleA_radius, {
       editable: true,
       minRadius: 1500,
       fillColor: '#29AB87'
     }).addTo(map);
 
-    var circleB = new MapboxCircle({lat: this.INIT_COORDS_B[1], lng: this.INIT_COORDS_B[0]}, 7000, {
+    var circleB = new MapboxCircle({lat: this.state.circleB_center[1], lng: this.state.circleB_center[0]}, this.state.circleB_radius, {
       editable: true,
       minRadius: 1500,
       fillColor: '#29AB87'
     }).addTo(map);
 
-    circleA.on('centerchanged', function (circleObj) {
-      console.log('New center:', circleObj.getCenter());
+    circleA.on('centerchanged', (circleObj) => {
+      this.setState({circleA_center: circleObj.getCenter()});
     });
-    circleA.once('radiuschanged', function (circleObj) {
-      console.log('New radius (once!):', circleObj.getRadius());
+    circleA.on('radiuschanged', (circleObj) => {
+      this.setState({circleA_radius: circleObj.getRadius()});
     });
-    // circleA.on('click', function (mapMouseEvent) {
-    //         console.log('Click:', mapMouseEvent.point);
-    //     });
+    circleB.on('centerchanged', (circleObj) => {
+      this.setState({circleB_center: circleObj.getCenter()});
+    });
+    circleB.on('radiuschanged', (circleObj) => {
+      this.setState({circleB_radius: circleObj.getRadius()});
+    });
+  }
+
+  getIntersectionPoints(circleA_center, circleA_radius, circleB_center, circleB_radius) {
+    var turf_circleA = turf.circle(circleA_center, circleA_radius);
+    var turf_circleB = turf.circle(circleB_center, circleB_radius);
+
+    var intersection = turf.intersect(turf_circleA, turf_circleB);
+
+    // IN PROGRESS
+    // let restaurants = geodata["features"];
+    // let points = []
+
+    // for (var r of restaurants) {
+    //   let point = r["geometry"]["coordinates"];
+    //   if (turf.booleanContains(intersection, point)) {
+    //     points.append(point);
+    //   }
+    // }
+
+    // console.log(points);
   }
 
   handleMarkers(map) {
