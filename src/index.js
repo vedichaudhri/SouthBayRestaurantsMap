3faec7cf2522bdf4a6a7094ff59ad20e4441d0ee
Victoria Ding <victoriading19@gmail.com>
2020-02-08 21:20:32 -0800

    fix small bug in intersection colors; update files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './site.css';
-//import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-//import { csv2geojson } from 'csv2geojson';
 import mapboxgl from 'mapbox-gl';
 import geodata from './geodata.js';
 import MapboxCircle from 'mapbox-gl-circle';
-import * as turf from '@turf/turf'
+import * as turf from '@turf/turf';
 
 mapboxgl.accessToken =
-'pk.eyJ1IjoidmVkaWNoYXVkaHJpIiwiYSI6ImNrNjM2NGpxYjAxaG8zbW1weTNuMmxydDkifQ.2Va6SpC54bm8IkG0SGZ8lw';
+  'pk.eyJ1IjoidmVkaWNoYXVkaHJpIiwiYSI6ImNrNjM2NGpxYjAxaG8zbW1weTNuMmxydDkifQ.2Va6SpC54bm8IkG0SGZ8lw';
 
+function filterBy(num, map) {
+  var filters = ['>=', 'Score', num];
+  map.setFilter('points', filters);
+
+  document.getElementById('safety_score_range').textContent = `Score >= ${num}`; //safety_score[num];
+}
 
 class Application extends React.Component {
-
-  INIT_COORDS_A = [-122.1430195, 37.44];
-  INIT_COORDS_B = [-122.2, 37.39];
-
   constructor(props) {
     super(props);
     this.handleCircles = this.handleCircles.bind(this);
     this.getIntersectionPoints = this.getIntersectionPoints.bind(this);
-    this.handleMarkers = this.handleMarkers.bind(this);
     this.state = {
       lng: -122.1430195,
       lat: 37.4418834,
@@ -32,13 +30,16 @@ class Application extends React.Component {
       circleB_center: [-122.2, 37.39],
       circleA_radius: 7000,
       circleB_radius: 7000,
-      intersectionPoints: this.getIntersectionPoints([-122.1430195, 37.44], 7000, [-122.2, 37.39], 7000),
+      intersectionPoints: this.getIntersectionPoints(
+        [-122.1430195, 37.44],
+        7000,
+        [-122.2, 37.39],
+        7000
+      )
     };
-    
   }
 
   componentDidMount() {
-    console.log(this.state);
     var bounds = [
       [-123.0397, 37.024], // Southwest coordinates
       [-120.881, 37.933033] // Northeast coordinates
@@ -60,174 +61,317 @@ class Application extends React.Component {
       });
     });
 
-    var canvas = map.getCanvasContainer();
-    // var circles = this.createGeoJSONCircles(this.INIT_COORDS_A, this.INIT_COORDS_B, 10, 10);
-
-    map.on('load', function() {
+    map.on('load', () => {
       map.addSource('points', {
         type: 'geojson',
         data: geodata
       });
 
       map.addLayer({
-        'id': 'points',
-        'type': 'circle',
-        'source': 'points',
-        'paint': {
-          'circle-color': {
-            property: "Score",
-            stops: [
-              [41, '#FF0000'],
-              [75, '#FFFF66'],
-              [100, '#238823']
-            ]
-          }
-        }
+        id: 'points',
+        type: 'circle',
+        source: 'points',
+      });
+
+      map.setPaintProperty('points', 'circle-color', [
+        'case',
+        [
+          'in',
+          ['get', 'Address'],
+          ['literal', this.state.intersectionPoints]
+        ],
+        'green',
+        'gray'
+      ]);
+
+      map.setPaintProperty('points', 'circle-opacity', ['case',
+        [
+          'in',
+          ['get', 'Address'],
+          ['literal', this.state.intersectionPoints]
+        ],
+        1,
+        0.5
+      ]);
+
+      filterBy(9, map);
+      document.getElementById('slider').addEventListener('input', function (e) {
+        let num = parseInt(e.target.value, 10);
+        filterBy(num, map);
       });
     });
 
-    map.on('click', 'points', function(e) {
+    map.on('click', 'points', function (e) {
       var coordinates = e.features[0].geometry.coordinates.slice();
       var description = e.features[0].properties.Name;
-       
+
       // Ensure that if the map is zoomed out such that multiple
       // copies of the feature are visible, the popup appears
       // over the copy being pointed to.
       while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
         coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
       }
-       
+
       new mapboxgl.Popup()
         .setLngLat(coordinates)
         .setHTML(description)
         .addTo(map);
     });
 
-    map.on('mouseenter', 'points', function() {
+    map.on('mouseenter', 'points', function () {
       map.getCanvas().style.cursor = 'pointer';
     });
-     
-    map.on('mouseleave', 'points', function() {
+
+    map.on('mouseleave', 'points', function () {
       map.getCanvas().style.cursor = '';
     });
 
-    // this.handleMarkers(map);
     this.handleCircles(map);
   }
 
   // radius in meters
   handleCircles(map) {
-    var circleA = new MapboxCircle({lat: this.state.circleA_center[1], lng: this.state.circleA_center[0]}, this.state.circleA_radius, {
-      editable: true,
-      minRadius: 1500,
-      fillColor: '#29AB87'
-    }).addTo(map);
-
-    var circleB = new MapboxCircle({lat: this.state.circleB_center[1], lng: this.state.circleB_center[0]}, this.state.circleB_radius, {
-      editable: true,
-      minRadius: 1500,
-      fillColor: '#29AB87'
-    }).addTo(map);
-
-    circleA.on('centerchanged', (circleObj) => {
-      this.setState({circleA_center: circleObj.getCenter()});
-    });
-    circleA.on('radiuschanged', (circleObj) => {
-      this.setState({circleA_radius: circleObj.getRadius()});
-    });
-    circleB.on('centerchanged', (circleObj) => {
-      this.setState({circleB_center: circleObj.getCenter()});
-    });
-    circleB.on('radiuschanged', (circleObj) => {
-      this.setState({circleB_radius: circleObj.getRadius()});
-    });
-  }
-
-  getIntersectionPoints(circleA_center, circleA_radius, circleB_center, circleB_radius) {
-    var turf_circleA = turf.circle(circleA_center, circleA_radius);
-    var turf_circleB = turf.circle(circleB_center, circleB_radius);
-
-    var intersection = turf.intersect(turf_circleA, turf_circleB);
-
-    // IN PROGRESS
-    // let restaurants = geodata["features"];
-    // let points = []
-
-    // for (var r of restaurants) {
-    //   let point = r["geometry"]["coordinates"];
-    //   if (turf.booleanContains(intersection, point)) {
-    //     points.append(point);
-    //   }
-    // }
-
-    // console.log(points);
-  }
-
-  handleMarkers(map) {
-    var markerA = new mapboxgl.Marker({
-      draggable: true
-    })
-    .setLngLat(this.INIT_COORDS_A)
-    .addTo(map);
-
-    var markerB = new mapboxgl.Marker({
-      draggable: true
-    })
-    .setLngLat(this.INIT_COORDS_B)
-    .addTo(map);
-
-    // function onDragEnd() {
-    //   var lngLatA = markerA.getLngLat();
-    //   var lngLatB = markerB.getLngLat();
-    // }
-
-    // markerA.on('dragend', onDragEnd);
-    // markerB.on('dragend', onDragEnd);
-  }
-
-  /* createGeoJSONCircles(centerA, centerB, radiusA, radiusB) {
-    let coordsA = this.calculateCircleCoords(centerA, radiusA);
-    let coordsB = this.calculateCircleCoords(centerB, radiusB);
-
-    return {
-      "type": "geojson",
-      "data": {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "geometry": {
-            "type": "Polygon",
-            "coordinates": [coordsA]
-          }
-        }, 
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Polygon",
-            "coordinates": [coordsB]
-          }
-        }]
+    var circleA = new MapboxCircle(
+      { lat: this.state.circleA_center[1], lng: this.state.circleA_center[0] },
+      this.state.circleA_radius,
+      {
+        editable: true,
+        minRadius: 1500,
+        fillColor: '#29AB87'
       }
-    };
-  }; */
+    ).addTo(map);
+
+    var circleB = new MapboxCircle(
+      { lat: this.state.circleB_center[1], lng: this.state.circleB_center[0] },
+      this.state.circleB_radius,
+      {
+        editable: true,
+        minRadius: 1500,
+        fillColor: '#29AB87'
+      }
+    ).addTo(map);
+
+    circleA.on('centerchanged', circleObj => {
+      var newCenter = [
+        circleObj.getCenter()['lng'],
+        circleObj.getCenter()['lat']
+      ];
+      this.setState(
+        {
+          circleA_center: newCenter,
+          intersectionPoints: this.getIntersectionPoints(
+            newCenter,
+            this.state.circleA_radius,
+            this.state.circleB_center,
+            this.state.circleB_radius
+          )
+        },
+        () => {
+          map.setPaintProperty('points', 'circle-color', [
+            'case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            'green',
+            'gray'
+          ]);
+          map.setPaintProperty('points', 'circle-opacity', ['case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            1,
+            0.5
+          ]);
+        }
+      );
+    });
+    circleA.on('radiuschanged', circleObj => {
+      this.setState(
+        {
+          circleA_radius: circleObj.getRadius(),
+          intersectionPoints: this.getIntersectionPoints(
+            this.state.circleA_center,
+            circleObj.getRadius(),
+            this.state.circleB_center,
+            this.state.circleB_radius
+          )
+        },
+        () => {
+          map.setPaintProperty('points', 'circle-color', [
+            'case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            'green',
+            'gray'
+          ]);
+          map.setPaintProperty('points', 'circle-opacity', ['case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            1,
+            0.5
+          ]);
+        }
+      );
+    });
+
+    circleB.on('centerchanged', circleObj => {
+      var newCenter = [
+        circleObj.getCenter()['lng'],
+        circleObj.getCenter()['lat']
+      ];
+
+      this.setState(
+        {
+          circleB_center: newCenter,
+          intersectionPoints: this.getIntersectionPoints(
+            this.state.circleA_center,
+            this.state.circleA_radius,
+            newCenter,
+            this.state.circleB_radius
+          )
+        },
+        () => {
+          map.setPaintProperty('points', 'circle-color', [
+            'case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            'green',
+            'gray'
+          ]);
+          map.setPaintProperty('points', 'circle-opacity', ['case',
+            [
+              'in',
+              ['get', 'Address'],
+              ['literal', this.state.intersectionPoints]
+            ],
+            1,
+            0.5
+          ])
+        }
+      );
+    });
+    
+    circleB.on('radiuschanged', circleObj => {
+      if (circleObj.getRadius() !== null) {
+        this.setState(
+          {
+            circleB_radius: circleObj.getRadius(),
+            intersectionPoints: this.getIntersectionPoints(
+              this.state.circleA_center,
+              circleObj.getRadius(),
+              this.state.circleB_center,
+              circleObj.getRadius()
+            )
+          },
+          () => {
+            map.setPaintProperty('points', 'circle-color', [
+              'case',
+              [
+                'in',
+                ['get', 'Address'],
+                ['literal', this.state.intersectionPoints]
+              ],
+              'green',
+              'gray'
+            ]);
+            map.setPaintProperty('points', 'circle-opacity', ['case',
+              [
+                'in',
+                ['get', 'Address'],
+                ['literal', this.state.intersectionPoints]
+              ],
+              1,
+              0.5
+            ]);
+          }
+        );
+      }
+    });
+  }
+
+  getIntersectionPoints(
+    circleA_center,
+    circleA_radius,
+    circleB_center,
+    circleB_radius
+  ) {
+    let options = { units: 'kilometers' };
+    let turf_circleA = turf.circle(
+      circleA_center,
+      circleA_radius / 1000,
+      options
+    );
+    let turf_circleB = turf.circle(
+      circleB_center,
+      circleB_radius / 1000,
+      options
+    );
+
+    let intersection = turf.intersect(turf_circleA, turf_circleB);
+
+    if (intersection == null) {
+      return [];
+    }
+
+    let restaurants = geodata['features'];
+    let r_in_intersect = [];
+
+    for (let r of restaurants) {
+      let coords = r['geometry']['coordinates'];
+      let point = turf.point(coords);
+
+      if (turf.booleanPointInPolygon(point, intersection)) {
+        r_in_intersect.push(r['properties']['Address']);
+      }
+    }
+    return r_in_intersect;
+  }
 
   render() {
     return (
       <div>
-      <div className="sidebarStyle">
-      <div>
-      Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
-      {this.state.zoom}
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
+            {this.state.zoom}
+          </div>
+        </div>
+        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+        <div class="map-overlay top">
+          <div class="map-overlay-inner">
+            <h2>Filter</h2>
+            <label id="safety_score_range"></label>
+            <input
+              id="slider"
+              type="range"
+              min="0"
+              max="99"
+              step="1"
+              defaultValue="0"
+            />
+          </div>
+        </div>
       </div>
-      </div>
-      <div ref={el => (this.mapContainer = el)} className="mapContainer" />
-      </div>);
-    }
+    );
   }
+}
 
-  ReactDOM.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById('app'));
 
-  // If you want your app to work offline and load faster, you can change
+// If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
